8807d09881fff75d72e1ab66035884a29f0c082a
YinShun <1719743788@qq.com>
2017-12-14 15:10:25 +0800

    java

---
+++ Hello_World.java
@@ -0,0 +1,9 @@
+package ys;
+
+public class Hello_World {
+	public static void main(String []args){
+		System.out.println("Hello World");
+		System.out.println("大疯狼");
+		System.out.println("bilibili");
+	}
+}
